248be337de824be1a5160c9dd42e0ed68ec3edab
Ethan Alvi <ethanthomasalvi@gmail.com>
2024-03-12 11:43:40 -0400

    Added profile pictures to answer posts

--- src/components/AnswerPost.js
+++ src/components/AnswerPost.js
@@ -6,7 +6,8 @@ const AnswerPost = ({ answerData }) => {
     return (
         <div className="bg-white p-4 shadow-md rounded-md max-w-lg mx-auto mt-4" >
             <div className="mb-4">
-                <p className="font-bold">{answerData.answerer}</p>
+                <img className="rounded-full, w-12, h-12" src={answerData.img} />
+                <p className="font-bold">{answerData.name}</p>
                 <p className="text-gray-500 text-sm ">{new Date(answerData.created).getFullYear()}</p>
             </div>
             <div >{answerData.answer}</div>
